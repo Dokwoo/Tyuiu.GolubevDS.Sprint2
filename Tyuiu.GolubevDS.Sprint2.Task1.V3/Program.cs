9922f991ddcf782b92f8fc05f73302ccec9c1855
голубев дима <maska.l.v@mail.ru>
2023-10-09 12:49:24 +0500

    Add project files.

---
+++ Tyuiu.GolubevDS.Sprint2.Task1.V3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GolubevDS.Sprint2.Task1.V3.Lib;
+namespace Tyuiu.GolubevDS.Sprint2.Task1.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил Голубев Д. С. | АСОиУБ 23-2";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #2                                                              *");
+            Console.WriteLine("* Тема: Логические операции                                              *");
+            Console.WriteLine("* Задание #1                                                             *");
+            Console.WriteLine("* Вариант #3                                                             *");
+            Console.WriteLine("* Выполнил: Голубев Дмитрий Сергеевич | АСОиУБ 23-2                      *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >= )       *");
+            Console.WriteLine("* последовательность можно чередовать                                    *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            
+            DataService ds = new DataService();
+            int a = 185;
+            int b = 316;
+            int c = 134;
+            int d = 134;
+
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("c = " + c);
+            Console.WriteLine("d = " + d);
+
+            for (int i = 0; i < 6; i++);
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
