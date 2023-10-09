b0325dad529d0e50cdc6270803c8692155b0f46a
голубев дима <maska.l.v@mail.ru>
2023-10-09 16:47:27 +0500

    Task4

---
+++ Tyuiu.GolubevDS.Sprint2.Task4.V16.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GolubevDS.Sprint2.Task4.V16.Lib;
+namespace Tyuiu.GolubevDS.Sprint2.Task4.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void VolidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 10;
+            double res = ds.Calculate(x, y);
+            double wait = 1.01;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void VolidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 10;
+            double y = 1;
+            double res = ds.Calculate(x, y);
+            double wait = 0.99;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.GolubevDS.Sprint2.Task4.V16.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GolubevDS.Sprint2.Task4.V16.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GolubevDS.Sprint2.Task4.V16.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fd4747e0-cebe-42e1-ac76-fe1da718e759")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
